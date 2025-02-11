003e6c9dd9ddec608ead1799b1d190b4051cc8cd
Phani-kp <Phaneendhrak0427@gmail.com>
2025-02-11 13:40:17 -0600

    Update 1.access_adls_using_access_keys.py

--- 1.access_adls_using_access_keys.py
+++ 1.access_adls_using_access_keys.py
@@ -1,6 +1,12 @@
+# Set Azure Data Lake account key
 spark.conf.set(
     "fs.azure.account.key.formula1dl.dfs.core.windows.net",
-    "")
+    "YOUR_ACCOUNT_KEY"
+)
+
+# List files in the 'demo' container
 display(dbutils.fs.ls("abfss://demo@formula1dl.dfs.core.windows.net"))
+
+# Read data from 'circuits.csv' in the 'demo' container
 display(spark.read.csv("abfss://demo@formula1dl.dfs.core.windows.net/circuits.csv"))
 
